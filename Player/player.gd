ba5a590c8b7e5b89d237a1d3c0ae669ce212519a
J-AI4 <aishaani.jana@gmail.com>
2025-08-09 07:37:18 -0400

    Updated Quick Platformer
    Built the player.

---
+++ Player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var speed = 200
+var jump_force = -400
+var gravity = 900
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	var input_dir = Input.get_axis("ui_left", "ui_right")
+	velocity.x = input_dir * speed
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_force
+	
+	move_and_slide()
