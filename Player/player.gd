d0ec0a2e3d70f643a7fb21162c2f4bbf67523798
J-AI4 <aishaani.jana@gmail.com>
2025-08-09 13:29:23 -0400

    Almost complete
    edited player and added background

--- Player/player.gd
+++ Player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 var speed = 800
-var jump_force = -1200
-var gravity = 1000
+var jump_force = -2500
+var gravity = 5000
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -13,5 +13,5 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_force
-	
+			
 	move_and_slide()
